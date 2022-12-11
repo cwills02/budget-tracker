56ef9d09a7bb130c28daace56fa20a0eed903832
Corey <cwills788@gmail.com>
2022-12-10 23:24:05 -0600

    can add items to a list that renders to the screen

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   const [amountInput, setAmountInput] = useState('')
   const [descriptionInput, setDescriptionInput] = useState('')
   const [total, setTotal] = useState(0)
+  const [items, setItems] = useState([])
+  console.log(items)
+
+  const addItemHandler = () => {
+    const newItem = {description: descriptionInput, amount: amountInput}
+    if(amountInput !== '' && descriptionInput !== '') {
+      setItems([...items, newItem])
+    }
+  }
 
   const amountInputHandler = (e) => {
     if(!isNaN(Number(e.target.value))) {
@@ -20,14 +29,35 @@ function App() {
     const amountIncrement = amountInput
     const newTotal = total + amountIncrement
     setTotal(newTotal)
+    addItemHandler()
     setAmountInput('')
+    setDescriptionInput('')
   }
 
   const decrement = () => {
     const amountDecrement = amountInput
     const newTotal = total - amountDecrement
     setTotal(newTotal)
+    addItemHandler()
+    setDescriptionInput('')
     setAmountInput('')
+  }
+
+  let totalColor;
+  if(total >= 0) {
+    totalColor = 'green'
+  } else {
+    totalColor ='red'
+  }
+
+  let itemList;
+  if(items !== []) {
+    itemList = items.map((item, index) => (
+      <div key={index} className='item-list'>
+        <h4 className='item-description'>{item.description}</h4>
+        <h4 className='item-amount'>${item.amount}</h4>
+      </div>
+    ))
   }
 
   return (
@@ -49,7 +79,10 @@ function App() {
           <button onClick={decrement}>-</button>
         </div>
       </div>
-      <h2>${total}</h2>
+      <h2>Total Budget: <span style={{color: totalColor}}>${total}</span></h2>
+      <div className='list-container'>
+        {itemList}
+      </div>
     </div>
   );
 }
