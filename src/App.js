a605791c0e715b9d070dad0838b854c9071d20fb
Corey <cwills788@gmail.com>
2022-12-12 09:34:00 -0600

    basic functionality works with adding, removing items, and deleting items from the list, with total being added or subracted from

--- src/App.js
+++ src/App.js
@@ -6,41 +6,29 @@ function App() {
   const [descriptionInput, setDescriptionInput] = useState('')
   const [total, setTotal] = useState(0)
   const [items, setItems] = useState([])
-  console.log(items)
 
   const addItemHandler = () => {
     const newItem = {description: descriptionInput, amount: amountInput}
     if(amountInput !== '' && descriptionInput !== '') {
       setItems([...items, newItem])
+      setTotal(total + Number(amountInput))
     }
+    setAmountInput('')
+    setDescriptionInput('')
+  }
+
+  const removeItemHandler = (listItem) => {
+    setTotal(total - listItem.amount)
+    const newItems = items.filter(item => item !== listItem)
+    setItems(newItems)
   }
 
   const amountInputHandler = (e) => {
-    if(!isNaN(Number(e.target.value))) {
-      setAmountInput(Number(e.target.value))
-    }
+      setAmountInput(e.target.value)
   }
 
   const descriptionInputHandler = (e) => {
     setDescriptionInput(e.target.value)
-  }
-
-  const increment = () => {
-    const amountIncrement = amountInput
-    const newTotal = total + amountIncrement
-    setTotal(newTotal)
-    addItemHandler()
-    setAmountInput('')
-    setDescriptionInput('')
-  }
-
-  const decrement = () => {
-    const amountDecrement = amountInput
-    const newTotal = total - amountDecrement
-    setTotal(newTotal)
-    addItemHandler()
-    setDescriptionInput('')
-    setAmountInput('')
   }
 
   let totalColor;
@@ -56,6 +44,12 @@ function App() {
       <div key={index} className='item-list'>
         <h4 className='item-description'>{item.description}</h4>
         <h4 className='item-amount'>${item.amount}</h4>
+        <button 
+          className='remove-item'
+          onClick={() => removeItemHandler(item)}
+        >
+          X
+        </button>
       </div>
     ))
   }
@@ -68,6 +62,7 @@ function App() {
           placeholder='Add amount'
           onChange={amountInputHandler}
           value={amountInput}
+          type='number'
         />
         <input 
           placeholder='Add Description'
@@ -75,8 +70,7 @@ function App() {
           value={descriptionInput}
         />
         <div>
-          <button onClick={increment}>+</button>
-          <button onClick={decrement}>-</button>
+          <button onClick={addItemHandler}>Add Item</button>
         </div>
       </div>
       <h2>Total Budget: <span style={{color: totalColor}}>${total}</span></h2>
